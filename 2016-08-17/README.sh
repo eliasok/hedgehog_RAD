23c3bc32ca4a3d1c0ce33cbb2e7deb13db1c5e50
eliasok <kristyna.eliasova@natur.cuni.cz>
2016-08-17 16:44:19 +0200

    Processing reads by SAMtools and removing mitochondrial sequences

--- 2016-08-17/README.sh
+++ 2016-08-17/README.sh
@@ -8,8 +8,8 @@
 #A necessary step at this point is filtering out mitochondrial sequences using SAMtools view 
 #once more. When NC_002080.2 is a name of cytochrome sequence I will concatenate all headers @SQ 
 #with defined C(name)and L(lenght) and paste them to z1 file. Then I will remove NC_002080.2
-#name out of list and use z1 file to show me all sequences contains everything apart of mtch sequence.
-#Then I will index sorted nuclear sequences.
+#name out of list and use z1 file to show me all sequences contains everything apart of mtch 
+#sequence.Then I will index sorted nuclear sequences.
 
 samtools view -H Er51_436_sorted.bam | grep "@SQ" | gawk '{C=substr($2,4); L=substr($3,4); print C "\t" 1 "\t" L}' > z1
 
