ff903c08a7cfdff45763b4c49fd70893726e388b
eliasok <kristyna.eliasova@natur.cuni.cz>
2019-12-06 13:55:06 +0100

    Delete gstacks2.sh

--- gstacks2.sh
+++
@@ -1,4 +0,0 @@
-DATADIR=~/hedgehog/results_2018/17-04-2018
-BAM=~/hedgehog/data_2018/merged_bam
-
-gstacks -I $BAM -O $DATADIR -M $DATADIR/popmap --rm-unpaired-reads --rm-pcr-duplicates --min-mapq 30 -t 5 --details
